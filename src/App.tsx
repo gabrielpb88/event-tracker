84c2611b81a42839695621876ec8a3bac884bc8c
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-01-02 21:25:38 -0300

    refact: removido prop drilling da alteração de evento

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,6 @@ function App() {
 
   const [filtro, setFiltro] = useState<Date | null>();
 
-  const alterarStatusEvento = (id: number) => {
-    const evento = eventos.find((evento) => evento.id === id);
-    if (evento) {
-      evento.completo = !evento.completo;
-    }
-    setEventos([...eventos]);
-  };
-
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data);
   };
@@ -41,10 +33,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos
-              aoFiltroAplicado={aplicarFiltro}
-              aoAlterarStatus={alterarStatusEvento}
-            />
+            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
           </Card>
         </div>
         <div>
--- src/components/Evento/EventoCheckbox/index.tsx
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { IEvento } from '../../../interfaces/IEvento';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../../state';
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
-  
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
+  const setListaDeEventos = useSetRecoilState(listaDeEventosState);
+  const alterarStatus = () => {
+    const eventoAlterado = {
+      ...evento,
+      completo: !evento.completo,
+    };
+
+    setListaDeEventos((prevState) => {
+      const index = prevState.findIndex((e) => e.id === evento.id);
+      return [
+        ...prevState.slice(0, index),
+        eventoAlterado,
+        ...prevState.slice(index + 1),
+      ];
+    });
+  };
   const estilos = [
     'far',
     'fa-2x',
-    evento.completo ? 'fa-check-square' : 'fa-square'
-  ]
+    evento.completo ? 'fa-check-square' : 'fa-square',
+  ];
 
-  return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
-}
+  return <i className={estilos.join(' ')} onClick={alterarStatus}></i>;
+};
 
-export default EventoCheckbox
+export default EventoCheckbox;
